cf915498e38892bfc2323d707854191f37c2cd42
Taivo Käsper <taivo.kasper@gmail.com>
2018-02-19 16:24:26 +0200

    Added a cheap integration test that uses bash to invoke the binary and validate the output

---
+++ integration-tests.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+binary=target/debug/unzipr
+if [ ! -e "$binary" ]; then
+    binary=target/release/unzipr
+fi
+
+# Test 1
+# Test listing files in a zip file
+output=`$binary -l tests/resources/test.zip`
+expected="test/
+test/test.txt"
+
+if [ "$output" != "$expected" ]; then
+    echo "'$output' does not equal '$expected'"
+    exit 1
+fi
+
+# Test 2
+# Test listing files in nested zip file
+output=`$binary -l tests/resources/test-test.zip test.zip`
+expected="test/
+test/test.txt"
+
+if [ "$output" != "$expected" ]; then
+echo "'$output' does not equal '$expected'"
+    exit 1
+fi
