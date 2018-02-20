456e3145dfddda2569fc69fc13a0326ca5a66a68
Taivo Käsper <taivo.kasper@gmail.com>
2018-02-20 18:02:58 +0200

    Return in-memory ByteArchive from recursive method so we can apply whatever logic we want to the nested zip result
    Got it compiling only thanks to Dmitri Gabbasov and his knowledge about
    Rc

--- src/main.rs
+++ src/main.rs
@@ -8,45 +8,25 @@ use std::fs::File;
 use zip::ZipArchive;
 use std::io::BufWriter;
 use std::io::Cursor;
+use std::io::Read;
+use std::rc::Rc;
 
-struct FileArchive<'a> {
-    file_path: &'a Path
+type ByteArchive = ZipArchive<Cursor<Vec<u8>>>;
+
+fn new_from_file(zip_file_path: &Path) -> ByteArchive {
+    let mut opened_file: File = File::open(&zip_file_path).unwrap();
+
+    let mut data = Vec::new();
+    opened_file.read_to_end(&mut data).unwrap();
+
+    return ZipArchive::new(Cursor::new(data)).unwrap();
 }
 
-impl<'a> FileArchive<'a> {
-    fn new(zip_file_path: &'a Path) -> FileArchive {
-        return FileArchive { file_path: &zip_file_path }
-    }
+fn new_from_bytes(bytes: Vec<u8>) -> ByteArchive {
+    return ZipArchive::new(Cursor::new(bytes)).unwrap();
 }
 
-struct BytesArchive {
-    bytes: Vec<u8>
-}
-
-impl BytesArchive {
-    fn new(archive_bytes: Vec<u8>) -> BytesArchive {
-        return BytesArchive { bytes: archive_bytes }
-    }
-}
-
-impl Archive<Cursor<Vec<u8>>> for BytesArchive {
-    fn to_zip_archive(&self) -> ZipArchive<Cursor<Vec<u8>>> {
-        return ZipArchive::new(Cursor::new(self.bytes.clone())).unwrap();
-    }
-}
-
-impl<'a> Archive<File> for FileArchive<'a> {
-    fn to_zip_archive(&self) -> ZipArchive<File> {
-        let opened_file: File = File::open(&self.file_path).unwrap();
-        return ZipArchive::new(opened_file).unwrap();
-    }
-}
-
-trait Archive<T: std::io::Read + io::Seek> {
-    fn to_zip_archive(&self) -> ZipArchive<T>;
-}
-
-fn get_files_list<R: std::io::Read + io::Seek>(archive: &mut ZipArchive<R>) -> Vec<String> {
+fn get_files_list(archive: &mut ByteArchive) -> Vec<String> {
     let mut name_vec = Vec::new();
     for i in 0..archive.len() {
         let file = archive.by_index(i).unwrap();
@@ -57,8 +37,8 @@ fn get_files_list<R: std::io::Read + io::Seek>(archive: &mut ZipArchive<R>) -> V
 
 #[test]
 fn test_get_files_list() {
-    let mut test_archive = FileArchive::new(Path::new("tests/resources/test.zip")).to_zip_archive();
-    let files_list = get_files_list(&mut test_archive);
+    let mut archive = new_from_file(Path::new("tests/resources/test.zip"));
+    let files_list = get_files_list(&mut archive);
 
     assert_eq!(2, files_list.len());
     assert_eq!("test/", files_list[0]);
@@ -86,12 +66,12 @@ fn main() {
     let files: Vec<&str> = matches.values_of("files").unwrap().collect();
 
     let source_file = Path::new(files[0]);
-    let file_archive = FileArchive::new(source_file);
+    let archive = new_from_file(source_file);
 
     if list {
         let rec_files = files[1..].to_vec();
-        let files_list = parse_files_rec(&mut file_archive.to_zip_archive(), &rec_files);
-        for file_name in files_list {
+        let mut inner_archive = parse_files_rec(Rc::new(archive), &rec_files);
+        for file_name in get_files_list(Rc::get_mut(&mut inner_archive).unwrap()) {
             println!("{}", file_name);
         }
     } else {
@@ -99,28 +79,29 @@ fn main() {
     }
 }
 
-fn parse_files_rec<R: std::io::Read + io::Seek>(archive: &mut ZipArchive<R>, rec_files: &Vec<&str>) -> Vec<String> {
+fn parse_files_rec(mut archive: Rc<ByteArchive>, rec_files: &Vec<&str>) -> Rc<ByteArchive> {
     if rec_files.is_empty() {
-        return get_files_list(archive);
+        return archive;
     } else {
         let source_file = rec_files[0];
         let deep_files = rec_files[1..].to_vec();
 
-        let mut file = archive.by_name(source_file).unwrap();
+        let mut file = Rc::get_mut(&mut archive).unwrap().by_name(source_file).unwrap();
 
         let mut buf = Vec::new();
         io::copy(&mut file, &mut BufWriter::new(&mut buf)).unwrap();
 
-        let bytes_archive = BytesArchive::new(buf);
-        return parse_files_rec(&mut bytes_archive.to_zip_archive(), &deep_files);
+        let new_archive = new_from_bytes(buf);
+        return parse_files_rec(Rc::new(new_archive), &deep_files);
     }
 }
 
 #[test]
 fn test_parsing_files_in_nested_zip() {
-    let mut test_archive = FileArchive::new(Path::new("tests/resources/test-test.zip")).to_zip_archive();
+    let test_archive = new_from_file(Path::new("tests/resources/test-test.zip"));
     let nested_archives = vec!["test.zip"];
-    let files_list = parse_files_rec(&mut test_archive, &nested_archives);
+    let mut archive = parse_files_rec(Rc::new(test_archive), &nested_archives);
+    let files_list = get_files_list(Rc::get_mut(&mut archive).unwrap());
 
     assert_eq!(2, files_list.len());
     assert_eq!("test/", files_list[0]);
